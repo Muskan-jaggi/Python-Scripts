3e798678ca25137662de97fbf51a99eaef51b698
Muskan Jaggi <muskanjaggi7@gmail.com>
2024-09-10 16:23:41 +0530

    Delete python projects directory

--- python projects/Tip Calculator/main.py.py
+++
@@ -1,9 +0,0 @@
-print("WELCOME TO THE TIP CALCULATOR")
-bill= float(input("what was the total bill? Rs."))
-percent= int(input("what percentage of tip you would like to give? 10, 12, or 15? "))
-split= int(input("how many people to split the bill? "))
-tip = percent/100
-tipvalue= bill * tip
-total= bill + tipvalue
-each= total/split
-print(f"each person should pay = {each:.2f}")
